5a39e964a8d7e3986f91ab9a04a8980d0e832a19
MilenK <milen_kunchev@abv.bg>
2022-08-01 22:27:11 +0300

    update

--- The observed PIN/the_observed_PIN.py
+++ The observed PIN/the_observed_PIN.py
@@ -1,15 +1,17 @@
 def get_pins(observed):
+    count_digits = len(str(observed))
+
     def get_possible_digits(noted_pin):
         possible = {
             '1': ['1', '2', '4'],
             '2': ['1', '2', '3', '5'],
-            '3': ['2', '6'],
-            '4': ['1', '5', '7'],
+            '3': ['2', '3', '6'],
+            '4': ['1', '4', '5', '7'],
             '5': ['2', '4', '5', '6', '8'],
             '6': ['3', '5', '6', '9'],
             '7': ['4', '7', '8'],
-            '8': ['8', '0', '5', '7', '9'],
-            '9': ['8', '9', '6'],
+            '8': ['0', '5', '7', '8', '9'],
+            '9': ['6', '8', '9'],
             '0': ['0', '8']
         }
         result = []
@@ -18,15 +20,22 @@ def get_pins(observed):
             result.append(possible[char])
         return result
 
-    def gen_pins():
-        return None
+    def gen_pins(idx, digits, pin, all_pins):
+        if idx == count_digits:
+            return
+        for combination in digits:
+            # TODO variations from nums in digits
+            gen_pins(idx + 1, digits, pin, all_pins)
 
     possible_digits = get_possible_digits(observed)
-    return get_possible_digits(observed)
+    gen_pins(0, possible_digits, [], [])
+
+    return None
 
 
-print(get_pins(1911))
+print(get_pins(369))
 # expected = sorted(
-#     ['20', '25', '27', '28', '29', '40', '45', '47', '48', '49', '50', '55', '57', '58',
-#      '59', '60', '65', '67', '68', '69', '80', '85', '87', '88', '89'])
+#     ["339","366","399","658","636","258","268","669","668","266","369","398","256","296","259","368",
+#     "638","396","238","356","659","639","666","359","336","299","338","696","269","358","656","698","699","298","236","239"])
+
 # print(expected)
